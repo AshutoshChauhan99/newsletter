a2af5fc755ce17499be882d89595acc736798de5
Ashutosh Chauhan <ashutoshrajput954@gmail.com>
2022-05-25 10:12:01 +0530

    commit project

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const res = require("express/lib/response");
+const https = require("https");
+const { options } = require("request");
 
 
 
@@ -20,16 +22,66 @@ app.get("/",function(req, res){
 });
 
 app.post("/", function(req,res){
-var firstName = req.body.fName;
-var lasttName = req.body.lName;
-var email = req.body.email;
-console.log(firstName, lasttName, email);
+const firstName = req.body.fName;
+const lastName = req.body.lName;
+const email = req.body.email;
+
+const data = {
+    members: [
+        {
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName,
+            }
+        }
+    ]
+};
+
+const jsonData = JSON.stringify(data);
+
+const url = "https://us11.api.mailchimp.com/3.0/lists/6542288d95";
+
+const options = {
+    method: "POST",
+    auth: "ashutosh:448f85930bd7dc4e230c2f662bda41f5-us11"
+}
+
+const request = https.request(url, options, function(response){
+
+   if (response.statusCode ===200) {
+       res.sendFile(__dirname + "/success.html");
+   }
+   else{
+    res.sendFile(__dirname + "/failure.html");
+   }
+    response.on("data", function(data){
+        console.log(JSON.parse(data));
+    })
+
+})
+
+request.write(jsonData);
+request.end();
 
 });
 
+app.post("/failure", function(req, res){
+    res.redirect("/")
+})
 
-app.listen(8080, function(){
+
+
+
+app.listen(process.env.PORT || 8080, function(){
 
     console.log("Server is running on port 8080")
 
 });
+
+
+// API KEY
+// 448f85930bd7dc4e230c2f662bda41f5-us11
+// audience id
+// 6542288d95
